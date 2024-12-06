bbfba18bb5a36d4b8da2ec98085519e7a58cf091
JJ_TheDragon <jbjonjernigan@gmail.com>
2024-12-05 19:54:05 -0600

    Day 3 part 2

--- day3/part2.py
+++ day3/part2.py
@@ -1,8 +1,31 @@
 import re
+
+
+def getMulNumbersList(tempStr):
+    return re.findall(r"mul\((\d+),(\d+)\)", tempStr)
+
+
+def splitDo(tempStr):
+    return re.split(r"do\(\)", tempStr)
+
+
+def splitDont(tempStr):
+    return re.split(r"don't\(\)", tempStr)
+
+
 sum = 0
 with open("input.txt") as testInput:
-    for lines in testInput:
-        regOutput = re.findall(r"mul\((\d+),(\d+)\)", lines)
-        for i, j in regOutput:
-            sum += int(i) * int(j)
-print(sum)
+    textInfor = testInput.read()
+    textInfor.replace("\n", "")
+    dontSplit = splitDont(textInfor)
+    procFirst = getMulNumbersList(dontSplit[0])
+    for i, j in procFirst:
+        sum += int(i) * int(j)
+    for strSplit in dontSplit[1:]:
+        doSplit = splitDo(strSplit)
+        if len(doSplit) is not None:
+            for finSplit in doSplit[1:]:
+                listMul = getMulNumbersList(finSplit)
+                for i, j in listMul:
+                    sum += int(i) * int(j)
+    print(sum)
